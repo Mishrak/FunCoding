07ad7ef80334b4e106c1a22f6af453d6e17042b6
Mishrak <mishraksharma1991@gmail.com>
2017-07-29 15:32:54 +0530

    Added comments and header file for better understanding.
    Signed-off-by: Mishrak <mishraksharma1991@gmail.com>

--- common.c
+++ common.c
@@ -1,85 +1,65 @@
-#include "birthday.h"
+/********************************************************************************/
+/* Before using any function read the instructions written above that fucntion  */
+/* Modify according to your need and enjoy coding                               */
+/********************************************************************************/
 
+#include "common.h"
 
+/***************************************************************/
+/* Description : This Function is printing only one character  */
+/* Linked      : Linked with display function                  */
+/* argument    : char x    : one character                     */
+/* return      : void      : nothing                           */
+/***************************************************************/
 void print_char(char x)
 {
    printf("%c", x);
    fflush(stdout);
 }
 
-void terminate()
-{
-   int i, j;
-   int n = 34;
-   system("clear");
+/***************************************************************/
+/* Description : This fucntion is for providing delay          */
+/*               in second, mili second or micro second        */
+/* Linked      : None                                          */
+/* argument    : time      : number i.e 1, 2, etc ...          */
+/*               num       : unit of delay(use MACRO)          */
+/*                         : i.e MICRO_SECOND,MILI_SECOND,etc..*/
+/* return      : int       : status : PASS or FAIL             */
+/***************************************************************/
 
-   display("===============>>>>>>>>>>    THANK YOU    <<<<<<<<<<===============\n",MILI_SECOND_100);
-   for(i=n/2; i<=n; i+=2)
-   {
-      for(j=1; j<n-i; j+=2)
-      {
-         display(" ",MILI_SECOND_5);
-      }
-
-      for(j=1; j<=i; j++)
-      {
-         display("*",MILI_SECOND_5);
-      }
-
-      for(j=1; j<=n-i; j++)
-      {
-         display(" ",MILI_SECOND_5);
-      }
-
-      for(j=1; j<=i; j++)
-      {
-         display("*",MILI_SECOND_5);
-      }
-
-      display("\n",MILI_SECOND_5);
+int nap(long time, int num) {
+   int status        =  SUCCESS;
+   switch(num) {
+      case MICRO_SECOND:
+         usleep(time);
+         break;
+      case MILI_SECOND:
+         usleep(time * 1000);
+         break;
+      case SECOND:
+         sleep(time);
+         break;
+      default:
+         status = FAIL;
+         break;
    }
-
-   for(i=n; i>=1; i--)
-   {
-      for(j=i; j<n; j++)
-      {
-         display(" ",MILI_SECOND_5);
-      }
-
-      for(j=1; j<=(i*2)-1; j++)
-      {
-         display("*",MILI_SECOND_5);
-      }
-
-      display("\n",MILI_SECOND_5);
-   }
-   exit(0);
+   return status;
 }
 
-void display(char *a, int num)
+/***************************************************************/
+/* Description : This fucntion is for printing string character*/
+/*               After perticular interval of time.            */
+/* Linked      : None                                          */
+/* argument    : char *a   : Input String                      */
+/*               delay     : delay in mili second              */
+/* return      : void      : nothing                           */
+/***************************************************************/
+void display(char *a, int delay)
 {
    int i = 0;
    for(i=0; a[i]; i++)
    {
       print_char(a[i]);
-      nap(num,MILI_SECOND);
+      nap(delay,MILI_SECOND);
    }
-}
-void not_in_the_list()
-{
-   system("clear");
-   printf("##################################################################\n");
-   display("## You have not selected any Wish from Birthday Wishes Box.\t##\n",MILI_SECOND_100);
-   display("## But you are very special for me.\t\t\t\t##\n",MILI_SECOND_100);
-   display("## Hence this one is for you my L",MILI_SECOND_100);
-   nap(1, SECOND);
-   display("ioness\t\t\t\t##\n",MILI_SECOND_100);
-   printf("##################################################################\n");
-   printf("\n\n\n\n\n\n\n");
-   printf("#############################################################################\n");
-   system("figlet HAPPY\ BIRTHDAY\ DEAR\ YESHA");
-   printf("#############################################################################\n");
-   printf("\nHAPPY BIRTHDAY DEAR YESHA\n");
-   printf("\n\n\n\n\n\n\n\n");
-   sleep(8);
 }
